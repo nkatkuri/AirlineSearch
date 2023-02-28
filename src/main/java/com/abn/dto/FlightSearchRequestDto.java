1af8d64e224f5f934f54f7a3c80a94d052cf591d
nkatkuri <126566879+nkatkuri@users.noreply.github.com>
2023-02-28 19:41:29 +0530

    fix(search):modified controller
    modified request object and added request dto

---
+++ src/main/java/com/abn/dto/FlightSearchRequestDto.java
@@ -0,0 +1,40 @@
+package com.abn.dto;
+
+import org.springframework.lang.NonNull;
+
+public class FlightSearchRequestDto {
+	@NonNull
+	private String origin;
+	@NonNull
+	private String destination;
+	
+	@NonNull
+	private String departureDate;
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public String getDepartureDate() {
+		return departureDate;
+	}
+
+	public void setDepartureDate(String departureDate) {
+		this.departureDate = departureDate;
+	}
+	
+	
+
+}
--- src/main/java/com/abn/restcontroller/SearchController.java
+++ src/main/java/com/abn/restcontroller/SearchController.java
@@ -2,6 +2,7 @@ package com.abn.restcontroller;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.abn.dto.FlightSearchRequestDto;
 import com.abn.dto.FlightSearchResponseDto;
 import com.abn.entity.Flight;
 import com.abn.entity.SearchEntity;
@@ -29,37 +31,29 @@ public class SearchController {
 	public static Logger logger = LoggerFactory.getLogger(SearchController.class);
 
 	@Autowired
-	private FlightService flightService;
-	
-	@GetMapping(value = "/getFlight/{id}")
-	public ResponseEntity<?> getFlight(@PathVariable("id") Integer flightId) {
-		logger.debug("** getFlight() - Execution started. **");
-		Flight theFlight = flightService.getFlight(flightId);
-		if (theFlight != null)
-			return new ResponseEntity<>(theFlight, HttpStatus.OK);
-		logger.debug("** getFlight() - Execution completed. **");
-		return new ResponseEntity<>("There is no flight with id: " + flightId + " in this route.",
-				HttpStatus.NO_CONTENT);
-	}
+	private FlightService flightService;		
 
 	@PostMapping("/availableFlights")
-	public ResponseEntity<?> getFilteredFlights(@RequestBody Map<String, String> body) {
+	public ResponseEntity<?> getFilteredFlights(@RequestBody FlightSearchRequestDto searchRequest) {
 		logger.debug("** getFilteredFlights() - Execution started. **");
 		List<FlightSearchResponseDto> filteredFlights = null;
-		String origin = null;
-		String destination = null;
-		String departureDate = null;
+
 		try {
-			origin = body.get("origin");
-			destination = body.get("destination");
-			departureDate = body.get("departureDate");
-			logger.info("** getFilteredFlights() - Data successfully parsed **");
+			if(Objects.nonNull(searchRequest)) {
+			String origin = searchRequest.getOrigin();
+			String destination = searchRequest.getDestination();
+			String departureDate = searchRequest.getDepartureDate();
+			
+			filteredFlights = flightService.getFilteredFlights(destination, origin, departureDate);
+			}else {
+				return new ResponseEntity<>("Invalid input data",HttpStatus.BAD_REQUEST);
+			}
 		}
 		catch(Exception ex) {
 			logger.info("** getFilteredFlights() - Exception occured **");
 		}
 		
-		filteredFlights = flightService.getFilteredFlights(destination, origin, departureDate);
+		
 
 		logger.debug("** getFilteredFlights() - Execution completed. **");
 		return new ResponseEntity<>(filteredFlights, HttpStatus.OK);
